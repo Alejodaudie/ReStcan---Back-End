dc1ea75379f89a401352dc01c4a848c9a5314a3b
Alejodaudie <alejo.daudi@gmail.com>
2018-12-19 23:38:23 +0100

    put address and imageURL to the Restaurant json

--- data/restaurants.js
+++ data/restaurants.js
@@ -1,8 +1,8 @@
 const restaurantsData = [
     {
       name: "NO TAN NEGRO",
-      address: "direccion",
-      imageUrl: "https://i.imgur.com/eTmWoAN.png",
+      address: "Avinguda Diagonal, 640, 08017 Barcelona",
+      imageUrl: "https://media.timeout.com/images/102795438/1024/576/image.jpg",
     }
 ]
 
